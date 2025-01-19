af33a02ba9edbd482abb5e4dc4f84bbb65839114
Daniel Knaack <daniel.knaack@protonmail.com>
2025-01-19 13:06:54 +0100

    Clean up the example code

--- example.c
+++ example.c
@@ -17,61 +17,55 @@ static const char *title = "Simple example";
 static void
 errorCallback(int err, const char *desc)
 {
-    fprintf(stderr, "Error: %s\n", desc);
+    fprintf(stderr, "GLFW Error: %s\n", desc);
 }
 
 static void
 keyCallback(GLFWwindow *win, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(win, GLFW_TRUE);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(win, GLFW_TRUE);
+	}
 }
 
 int main(void)
 {
     glfwSetErrorCallback(errorCallback);
     if (!glfwInit()) {
-		exit(EXIT_FAILURE);
+		return 1;
 	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
     GLFWwindow *window = glfwCreateWindow(640, 480, title, NULL, NULL);
     if (!window) {
-		goto error;
+		glfwTerminate();
+		return 1;
 	}
 
-    glfwSetKeyCallback(window, keyCallback);
+	glfwSetKeyCallback(window, keyCallback);
 
     glfwMakeContextCurrent(window);
-    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
-		goto error;
-	}
-
+	gladLoadGL((GLADloadfunc)glfwGetProcAddress);
     glfwSwapInterval(1);
-
-	/*
-	 * Initialize the text API
-	 */
 
 	float fontHeight = 48;
 	GLuint font = gltCreateFont("OpenSans.ttf", fontHeight);
+	GLTbuffer buffer = {0};
 
 	while (!glfwWindowShouldClose(window)) {
 		int w, h;
 		glfwGetFramebufferSize(window, &w, &h);
 		glViewport(0, 0, w, h);
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		char text[] = "Hello, world!";
+		char *text = "Hello, world!";
 		float textWidth = gltMeasureTextWidth(text);
 
 		float x = floor(0.5f * (w - textWidth));
 		float y = floor(0.5f * (h - fontHeight));
-		GLTbuffer buffer = {0};
 		gltBindFont(font);
 		gltPushText(&buffer, x, y, text);
 		gltDraw(buffer);
@@ -80,10 +74,9 @@ int main(void)
 		glfwPollEvents();
     }
 
+	free(buffer.vertices);
+	free(buffer.indices);
     glfwDestroyWindow(window);
     glfwTerminate();
-    exit(EXIT_SUCCESS);
-error:
-	glfwTerminate();
-	exit(EXIT_FAILURE);
+	return 0;
 }
